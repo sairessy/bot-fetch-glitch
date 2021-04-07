f1ea0102b74e4b694216fa29744adc4479ef83c0
sairessy <sairessy@gmail.com>
2021-04-08 00:34:42 +0300

    initializing

---
+++ index.js
@@ -0,0 +1,13 @@
+const fetch = require('node-fetch');
+
+async function awake() {
+  try {
+    const ft = await fetch('https://freelancerr.glitch.me/awake');
+    const res = await ft.json();
+    console.log(res);
+  } catch (err) {
+    console.error(err);
+  }
+} 
+
+awake();
